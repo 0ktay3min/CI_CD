e6238b94a85ba3e49db370d9943148b474d91066
0ktay3min <oktay_emin@hotmail.com>
2020-12-19 10:46:30 -0600

    Files added

---
+++ src/main/java/com/simplilearn/calculcator/CalculcatorApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.calculcator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculcatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculcatorApplication.class, args);
+	}
+
+}
